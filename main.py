69b91fb156755d196b6c85f7e56bb91d0e66c85b
chiikritoo <anis.chikri27@gmail.com>
2026-06-22 18:49:59 +0200

    feat(main): add test functions for addition, subtraction, and scalar multiplication

--- main.py
+++ main.py
@@ -1,14 +1,50 @@
 import sympy as sp
 from Vector import Vector
 
-def main():
-    v1 = Vector([14, 57, 77])
-    v2 = Vector([14, 57])
+def test_addition():
+    print('Test addition (v+v)')
+    v1 = Vector([5, 10, 15])
+    v2 = Vector([15, 10, 5])
     try:
         v1.add(v2)
         print(v1)
     except ValueError as error:
         print('Error:', error)
+
+    print()
+
+
+def test_substract():
+    print('Test substract (v-v)')
+    v1 = Vector([5, 10, 15])
+    v2 = Vector([15, 10, 5])
+    try:
+        v1.sub(v2)
+        print(v1)
+    except ValueError as error:
+        print('Error:', error)
+
+    print()
+
+def test_mult_by_scalar():
+    print('Test mult by scalar (n*v)')
+    v1 = Vector([5, 10, 15])
+    try:
+        v1.scl(2)
+        print(v1)
+    except ValueError as error:
+        print('Error:', error)
+
+    print()
+
+
+def main():
+    test_addition()
+    test_substract()
+    test_mult_by_scalar()
+
+
+
 
 
 if __name__ == "__main__":

880447a5127727bb9d07fba814ee14fc518c152f
chiikritoo <anis.chikri27@gmail.com>
2026-06-22 18:41:43 +0200

    feat(vector): add Vector class with add and sub operations

---
+++ Vector.py
@@ -0,0 +1,26 @@
+from __future__ import annotations
+
+class Vector:
+	def	__init__(self, numbers: list) -> None:
+		self._n: list = numbers
+
+	def __str__(self) -> str:
+		return str(self._n)
+
+	@property
+	def n(self) -> list:
+		return self._n
+
+	@n.setter
+	def n(self, numbers: list) -> None:
+		self._n: list = numbers
+
+	def add(self, other: Vector) -> None:
+		if len(self.n) != len(other.n):
+			raise ValueError(f"Vectors have not the same length ({self.n} - {other.n})")
+		self.n = [n1 + n2 for n1, n2 in zip(self.n, other.n)]
+
+	def sub(self, other: Vector) -> None:
+		if len(self.n) != len(other.n):
+			raise ValueError(f"Vectors have not the same length ({self.n} - {other.n})")
+		self.n = [n1 - n2 for n1, n2 in zip(self.n, other.n)]

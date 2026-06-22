b385e3608ea94dfa001928244d63c64b12b97e6b
chiikritoo <anis.chikri27@gmail.com>
2026-06-22 22:23:31 +0200

    refactor(vector): normalize indentation to spaces and add __repr__ method

--- Vector.py
+++ Vector.py
@@ -1,29 +1,32 @@
 from __future__ import annotations
 
 class Vector:
-	def	__init__(self, numbers: list) -> None:
-		self._n: list = numbers
+    def __init__(self, numbers: list) -> None:
+        self._n: list = numbers
 
-	def __str__(self) -> str:
-		return str(self._n)
+    def __str__(self) -> str:
+        return str(self._n)
 
-	@property
-	def n(self) -> list:
-		return self._n
+    def __repr__(self) -> str:
+        return str(self._n)
 
-	@n.setter
-	def n(self, numbers: list) -> None:
-		self._n: list = numbers
+    @property
+    def n(self) -> list:
+        return self._n
 
-	def add(self, other: Vector) -> None:
-		if len(self.n) != len(other.n):
-			raise ValueError(f"Vectors have not the same length ({self.n} - {other.n})")
-		self.n = [n1 + n2 for n1, n2 in zip(self.n, other.n)]
+    @n.setter
+    def n(self, numbers: list) -> None:
+        self._n: list = numbers
 
-	def sub(self, other: Vector) -> None:
-		if len(self.n) != len(other.n):
-			raise ValueError(f"Vectors have not the same length ({self.n} - {other.n})")
-		self.n = [n1 - n2 for n1, n2 in zip(self.n, other.n)]
+    def add(self, other: Vector) -> None:
+        if len(self.n) != len(other.n):
+            raise ValueError(f"Vectors have not the same length ({self.n} - {other.n})")
+        self.n = [n1 + n2 for n1, n2 in zip(self.n, other.n)]
 
-	def scl(self, scalar: int) -> None:
-		self.n = [scalar * number for number in self.n]
+    def sub(self, other: Vector) -> None:
+        if len(self.n) != len(other.n):
+            raise ValueError(f"Vectors have not the same length ({self.n} - {other.n})")
+        self.n = [n1 - n2 for n1, n2 in zip(self.n, other.n)]
+
+    def scl(self, scalar: int) -> None:
+        self.n = [scalar * component for component in self.n]

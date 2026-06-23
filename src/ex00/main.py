4dc285053dda1e4cdf091d830dea1ca12c8abeec
chiikritoo <anis.chikri27@gmail.com>
2026-06-23 21:20:44 +0200

    refactor(ex00): move vector operation tests to ex00 module

---
+++ src/ex00/main.py
@@ -0,0 +1,51 @@
+from common.Vector import Vector
+
+
+def test_addition():
+    print("Test addition (v+v)")
+    v1 = Vector([5, 10, 15])
+    v2 = Vector([15, 10, 5])
+    try:
+        v1.add(v2)
+        print(v1)
+    except ValueError as error:
+        print("Error:", error)
+
+    print()
+
+
+def test_substract():
+    print(
+        "Test substract (v-v)"
+    )
+    v1 = Vector([4, 10, 15])
+    v2 = Vector([15, 10, 5])
+    try:
+        v1.sub(v2)
+        print(v1)
+    except ValueError as error:
+        print("Error:", error)
+
+    print()
+
+
+def test_mult_by_scalar():
+    print("Test mult by scalar (n*v)")
+    v1 = Vector([5, 10, 15])
+    try:
+        v1.scl(2)
+        print(v1)
+    except ValueError as error:
+        print("Error:", error)
+
+    print()
+
+
+def main():
+    test_addition()
+    test_substract()
+    test_mult_by_scalar()
+
+
+if __name__ == "__main__":
+    main()
